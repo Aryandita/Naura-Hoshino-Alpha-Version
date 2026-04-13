3278dc3ef7762b622ca4f01b93a520de24799b48
Aryandita Praftian <112795931+Aryandita@users.noreply.github.com>
2026-04-13 08:18:06 +0800

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,27 +1,28 @@
-const { Events } = require('discord.js');
-const botActivity = require('../config/bot-activity');
+const { Events, ActivityType } = require('discord.js');
+const env = require('../config/env');
 
 module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
+    name: Events.ClientReady,
+    once: true,
+    async execute(client) {
+        console.log(`\x1b[32m[🤖 SYSTEM]\x1b[0m Naura Versi 1.0.0 Online! Terhubung sebagai \x1b[36m${client.user.tag}\x1b[0m`);
 
-		// Initialize the music manager
-		client.musicManager.initialize();
+        // Daftar status yang akan diputar secara otomatis
+        const activities = [
+            { name: 'Ryaa ngoding 🖥', type: ActivityType.Watching },
+            { name: 'Economy & Minigames 🎮', type: ActivityType.Playing },
+            { name: 'Instruksi Ryaa 💫', type: ActivityType.Listening },
+            { name: 'Kehidupan Ryaa 💌', type: ActivityType.Watching }
+        ];
 
-		// Set the bot's presence
-		const activities = botActivity.status.rotateDefault;
-		if (activities.length > 0) {
-			let currentIndex = 0;
-			setInterval(() => {
-				const activity = activities[currentIndex];
-				client.user.setActivity(activity.name, { type: activity.type, url: activity.url });
-				currentIndex = (currentIndex + 1) % activities.length;
-			}, 15000); // Rotate every 15 seconds
-			console.log('[PRESENCE] Bot activity rotator has been set.');
-		} else {
-			console.log('[PRESENCE] No activities found to set.');
-		}
-	},
+        let i = 0;
+        setInterval(() => {
+            const activity = activities[i % activities.length];
+            client.user.setPresence({
+                activities: [activity],
+                status: 'online', 
+            });
+            i++;
+        }, 15000); // Ganti status setiap 15 detik
+    }
 };
