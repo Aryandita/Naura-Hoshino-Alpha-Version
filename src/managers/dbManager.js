a2903e2dc1343a8c5ced0fb5775394b3e6d728bb
Aryandita Praftian <112795931+Aryandita@users.noreply.github.com>
2026-04-13 12:44:09 +0800

    Update dbManager.js

--- src/managers/dbManager.js
+++ src/managers/dbManager.js
@@ -10,6 +10,7 @@ const sequelize = new Sequelize(
         port: env.DB_PORT,
         dialect: 'mysql',
         logging: false, 
+        pool: { max: 5, min: 0, acquire: 30000, idle: 10000 } // Membuat koneksi lebih stabil
     }
 );
 
@@ -21,8 +22,8 @@ const connectToDatabase = async () => {
         await sequelize.sync({ alter: true });
         console.log('\x1b[42m\x1b[30m ✨ SUCCESS \x1b[0m \x1b[32mTabel MySQL berhasil disinkronisasi.\x1b[0m');
     } catch (error) {
-        console.error('\x1b[41m\x1b[37m 💥 ERROR \x1b[0m \x1b[31mGagal terhubung ke MySQL:\x1b[0m', error);
-        throw error;
+        // Tampilkan error, tapi JANGAN matikan bot (Hapus throw error;)
+        console.error('\x1b[41m\x1b[37m 💥 ERROR \x1b[0m \x1b[31mGagal terhubung ke MySQL: Koneksi Ditolak (Refused).\x1b[0m');
     }
 };
 
